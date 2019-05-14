516e242ac60bb35e9be603cac125c7474d120d0e
Patrick Lehmann <patrick.lehmann@homes.com>
2019-05-14 16:26:02 -0400

    added more katas

---
+++ 7kyu/complementary_dna.js
@@ -0,0 +1,29 @@
+// Deoxyribonucleic acid (DNA) is a chemical found in
+// the nucleus of cells and carries the "instructions" for the development
+// and functioning of living organisms.
+
+// If you want to know more http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+// You have function with one side of the DNA (string, except for Haskell);
+// you need to get the other complementary side.
+// DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here http://rosalind.info/problems/list-view/ (source)
+
+// DNAStrand ("ATTGC") // return "TAACG"
+
+// DNAStrand ("GTAT") // return "CATA"
+
+const DNAStrand = dna => {
+  const result = [];
+  for (let char of dna.split('')) {
+    char === 'A' ? char = 'T' :
+    char === 'T' ? char = 'A' :
+    char === 'C' ? char = 'G' :
+    char === 'G' ? char = 'C' : 'Let\'s Go Islanders'
+    result.push(char);
+  }
+
+  return result.join('');
+};
